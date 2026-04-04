aa211ff57ca66b4101367db9f81e26c331df4433
Nainikap <nainikapathak6@gmail.com>
2026-04-04 14:53:21 +0530

    changed logo to square size

--- rfs-fitness/src/app/layout.tsx
+++ rfs-fitness/src/app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
         <head>
     <meta charSet="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/logo.jpeg" />
+    <link rel="icon" type="image/svg+xml" href="/logo.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Redefined Fitness Solution</title>
   </head>
